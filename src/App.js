e24751b6849d5f087484421a63bdd3d0a5b43a97
Kum Somi <54243544+kumsomi@users.noreply.github.com>
2021-09-26 18:28:56 +0530

    added like counter added like counter on the view page which gets incremented on every click.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import "./styles.css";
 
 var welcomeMessage = "HEY, Goodmorning ";
 // var username=prompt("Give me your name")
 var color = "blue";
 var username = "kum somi";
+
+//useState variable and setter
+
 export default function App() {
+  const [likeCounter, setLikeCounter] = useState(0);
+
+  function likeClickHandler() {
+    var likeCounters = likeCounter + 1;
+    setLikeCounter(likeCounters);
+  }
+
   return (
     <div className="App">
       <h1>
         {welcomeMessage}
         <span style={{ color: color }}>{username}</span>
       </h1>
+      <button onClick={likeClickHandler}>Like Me</button>
+      {likeCounter}
     </div>
   );
 }
